b480f50855b11a6c5d338894df1f5ee4e078a7a8
ELLucho74 <luiscastroiturbide@gmail.com>
2025-04-09 00:11:39 -0600

    RESTART2

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-// Asegúrate de tener ESTO al inicio de tu server.js
-app.use(express.json()); // Para Express 4.16+ (mejor que body-parser)
 
 const app = express();
 
@@ -19,17 +17,13 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://Luis_Castro_Iturbide:ju
 
 // Modelo de datos
 const DataSchema = new mongoose.Schema({
-
-  sensorID:  { type: Number,
- required: true },  accelerationX: { type:  Number, required: true },
-  accelerationY: { type:  Number, required: true },
-  accelerationZ: { type:  Number, required: true },
-  temperature: { type:  Number, required: true },
-  dominantFrequency: { type:  Number, required: true },
-  frequencyX:  { type: Number,  required: true },// opcional si quieres guardar todos los ejes
-  frequencyY: { type:  Number, required: true },
-  frequencyZ: { type:  Number, required: true },
-  magnitude: { type:  Number, required: true }, 
+  sensorID: Number,
+  accelerationX: Number,
+  accelerationY: Number,
+  accelerationZ: Number,
+  temperature: Number,
+  dominantFrequency: Number,
+  magnitude: Number,
   timestamp: { type: Date, default: Date.now }
 });
 
